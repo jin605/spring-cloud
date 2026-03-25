28f789244021daadfed76ed09bf1157c3418ce37
jin605 <jinddd3@gmail.com>
2026-03-25 10:06:22 +0900

    spring cloud init

---
+++ 01_ConfigServer/src/main/java/configserver/Application.java
@@ -0,0 +1,16 @@
+package configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+// 스프링 클라우드 컨피그 서버로 활성화 한다.
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
