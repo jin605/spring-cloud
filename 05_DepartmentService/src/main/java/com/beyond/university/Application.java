3afc9e88aa31abc7c59d3d253d3dd687885b6afa
jin605 <jinddd3@gmail.com>
2026-03-25 12:36:44 +0900

    feat : 실습 프로젝트 init

--- 05_DepartmentService/src/main/java/com/beyond/university/Application.java
+++ 05_DepartmentService/src/main/java/com/beyond/university/Application.java
@@ -1,18 +1,13 @@
-package configserver;
+package com.beyond.university;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-// 스프링 클라우드 컨피그 서버로 활성화 한다.
-@EnableConfigServer
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
-
     }
 
 }
