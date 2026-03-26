13767925f4ba857f938ae8ada83050bacac91d51
jin605 <jinddd3@gmail.com>
2026-03-26 10:42:10 +0900

    feat : 넷플릭스 유레카 클라이언트 추가

--- 06_SubjectService/src/main/java/com/beyond/university/api/DepartmentApiClient.java
+++ 06_SubjectService/src/main/java/com/beyond/university/api/DepartmentApiClient.java
@@ -2,6 +2,7 @@ package com.beyond.university.api;
 
 import com.beyond.university.department.model.dto.DepartmentResponseDto;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 import lombok.RequiredArgsConstructor;
 
 @Component
+// 애플리케이션 실행 중에 일부 설정을 변경하도록 허용하는 어노테이션
+@RefreshScope
 @RequiredArgsConstructor
 public class DepartmentApiClient {
 	private final RestTemplate restTemplate;
